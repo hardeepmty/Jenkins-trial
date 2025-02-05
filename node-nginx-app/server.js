ab2e3dbfa43b27c3bc908ea93a13b322a7596773
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2025-02-05 10:51:32 +0530

    testing the auto trigger

--- node-nginx-app/server.js
+++ node-nginx-app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! This is a Node.js app behind Nginx.Made some changes on main");
+    res.send("Hello, World! This is a Node.js app behind Nginx.");
 });
 
 app.listen(PORT, () => {
